29a510a86b14db8e12b27d2e9bed28d99d2c44e4
Ivan Torres <francisco.ivantf@hotmail.com>
2025-09-02 11:09:17 -0600

    Refactor routing and component structure
    - Updated app-routing.module.ts to define routes for home and pages, including lazy loading for the pages module.
    - Cleaned up app.component.html by removing placeholder content and retaining only the <router-outlet> directive.
    - Modified app.module.ts to ensure HttpClientModule is imported for making HTTP requests.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/pages/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'pages',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+  },
+  {
+    path: '**',
+    redirectTo: '/pages/home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
